d4ec42503332bb49fa215822c7a1f709dbc04bc5
RodriGBorges <gabii.borges1998@gmail.com>
2021-12-12 21:34:51 -0300

    Archivos y sidebar

---
+++ practica1/src/components/App.jsx
@@ -0,0 +1,13 @@
+import '../assets/css/app.css';
+import SideBar from './SideBar';
+
+function App() {
+  return (
+    <div className="App">
+      <SideBar />
+      <h1>Hola</h1>
+    </div>
+  );
+}
+
+export default App;
